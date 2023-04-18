f393b048296334ef260e2cfa370261496e5bfe33
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-04-18 06:22:28 +0000

    [update]課題3

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about'
   devise_for :users
   resources :users
-  resources :books, only: [:new,:index,:show,:edit]
+  resources :books, only: [:new,:index,:show,:edit,:create]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
