52d9016b96bfe353384193690d9c3a5b06f63b75
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-04-19 03:45:21 +0000

    [update]課題3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
-  def new
+  def index
+    @books = Book.all
     @book = Book.new
   end
 
@@ -14,11 +15,6 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
-  end
-
-  def index
-    @books = Book.all
-    @book = Book.new
   end
 
   def show
