fe64d19517ed2a4fc6b178bbcaa4a06c5d101c70
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-04-21 10:20:42 +0000

    [update]課題3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,12 @@ class UsersController < ApplicationController
   end
 
   def edit
+    is_matching_login_user
     @user = current_user
   end
 
   def update
+    is_matching_login_user
     @user = current_user
     if @user.update(user_params)
       redirect_to user_path(current_user.id)
@@ -27,5 +29,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name,:profile,:profile_image,:introduction)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
   end
 end
