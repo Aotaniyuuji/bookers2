913acc7ed9ba85b4d88d3cefa550b4ec55cc65e1
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-04-22 02:26:43 +0000

    [update]課題3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
     def after_sign_up_path_for(resource)
+      flash[:notice] = "welcome! You have signed up successfully."
       user_path(@user.id)
     end
 
