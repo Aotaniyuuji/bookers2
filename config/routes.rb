c915e3f0a5c510fad83e56a8cd7792976f0120b8
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-08-09 10:32:33 +0000

    [start]ゲストログイン機能復習

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   resources :users, only: [:index,:show,:edit,:update]
   resources :books, only: [:new,:index,:show,:edit,:create,:update,:destroy]
 
+  devise_scope :user do
+    post "users/guest_sign_in", to: "uesrs/sessions#guest_sign_in"
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
